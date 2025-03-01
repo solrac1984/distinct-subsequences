6e0c5dd1ac92d545b14a1de03267f6add8ec813b
Carlos <solraccfgm_1@hotmail.com>
2025-03-01 17:35:21 -0500

    Solucion con Programación Dinámica y Tabulación

--- src/main/java/com/example/DistinctSubsequencesSolver.java
+++ src/main/java/com/example/DistinctSubsequencesSolver.java
@@ -1,40 +1,26 @@
 package com.example;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class DistinctSubsequencesSolver {
-    private Map<String, Integer> memo;
-
-    public DistinctSubsequencesSolver() {
-        this.memo = new HashMap<>();
-    }
-
     public int countDistinctSubsequences(String s, String t) {
-        return countHelper(s, t, s.length(), t.length());
-    }
+        int m = s.length(), n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
 
-    private int countHelper(String s, String t, int m, int n) {
-        // Caso base: si t está vacío, hay una subsecuencia válida
-        if (n == 0) return 1;
-
-        // Caso base: si s está vacío y t no, no hay subsecuencias posibles
-        if (m == 0) return 0;
-
-        String key = m + "," + n;
-        if (memo.containsKey(key)) {
-            return memo.get(key);
+        // Llenamos la primera columna: Si t está vacío, siempre hay una subsecuencia válida
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
         }
 
-        int result;
-        if (s.charAt(m - 1) == t.charAt(n - 1)) {
-            result = countHelper(s, t, m - 1, n - 1) + countHelper(s, t, m - 1, n);
-        } else {
-            result = countHelper(s, t, m - 1, n);
+        // Llenamos la tabla de abajo hacia arriba
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
         }
 
-        // Guardamos en el mapa antes de retornar
-        memo.put(key, result);
-        return result;
+        return dp[m][n];
     }
 }
